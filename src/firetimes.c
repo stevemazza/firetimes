558e60bc89ab3a02c4b1b6f4596b68bf2d77a01d
stevemazza <stevemazza@users.noreply.github.com>
2014-04-21 03:21:59 -0700

    Some improvements
    - Bigger font
    - Push and hold Select to store time (prevents accidental write-over)
    - Double vibrate when time stored
    - Move to next category when time stored
    - Triple click Select to reset app

--- src/firetimes.c
+++ src/firetimes.c
@@ -1,6 +1,5 @@
 #include <pebble.h>
 
-
 static Window *window;
 
 static int Selected;
@@ -20,10 +19,18 @@ static char selectedText[5][30] = {
   "Selected: ON SCENE",
   "Selected: STOP",
   "Selected: RETURNING",
-  "Selected: ON STATION"
+  "Selected:ON STATION"
 };
 
 static char textLayerText[5][40];
+
+static void select_multi_click_handler(ClickRecognizerRef recognizer, void *context) {
+  for (int i=0; i<5; i++) {
+    text_layer_set_text(textLayers[i], prefixText[i]);
+  }
+  text_layer_set_text(tlSelected, selectedText[0]);
+
+}
 
 static void long_click_up_handler(ClickRecognizerRef recognizer, void *context) {
 }
@@ -43,6 +50,7 @@ static char CurrentTime[6];
   text_layer_set_text(textLayers[Selected], textLayerText[Selected]);
   Selected = (Selected+1)%5;
   text_layer_set_text(tlSelected, selectedText[Selected]);
+  vibes_double_pulse();
 }
 
 static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
@@ -59,6 +67,7 @@ static void click_config_provider(void *context) {
   window_long_click_subscribe(BUTTON_ID_SELECT, 700, long_click_handler, long_click_up_handler);
   window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
   window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
+  window_multi_click_subscribe(BUTTON_ID_SELECT, 3, 0, 0, false, select_multi_click_handler);
 }
 
 static void window_load(Window *window) {
@@ -66,15 +75,17 @@ static void window_load(Window *window) {
   GRect bounds = layer_get_bounds(window_layer);
   Selected = 0;
 
-  tlSelected = text_layer_create((GRect) { .origin = { 0, 10 }, .size = { bounds.size.w, 20 } });
+  tlSelected = text_layer_create((GRect) { .origin = { 0, 10 }, .size = { bounds.size.w, 23 } });
   text_layer_set_text(tlSelected, selectedText[0]);
+  text_layer_set_font(tlSelected, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
   text_layer_set_text_alignment(tlSelected, GTextAlignmentLeft);
   layer_add_child(window_layer, text_layer_get_layer(tlSelected));
 
   for (int i=0; i<5; i++) {
-    textLayers[i] = text_layer_create((GRect) { .origin = { 0, 30+20*i }, .size = { bounds.size.w, 20 } });
+    textLayers[i] = text_layer_create((GRect) { .origin = { 0, 30+23*i }, .size = { bounds.size.w, 23 } });
     text_layer_set_text(textLayers[i], prefixText[i]);
     text_layer_set_text_alignment(textLayers[i], GTextAlignmentLeft);
+    text_layer_set_font(textLayers[i], fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
     layer_add_child(window_layer, text_layer_get_layer(textLayers[i]));
   }
 }
